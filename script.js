77023a55606756ac4cab6b6af460102871ac597b
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-06-20 10:54:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const browserInfo = document.getElementById("browser-info");
+const browserName = navigator.userAgent;
+const browserVersion = navigator.appVersion;
+
+browserInfo.innerText = "You are using " + browserName + " version " + browserVersion;
